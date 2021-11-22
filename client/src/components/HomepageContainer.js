abfb5893807b04f4078270c016a529e70f2d2fd2
Basquiat <ememdaniel98@gmail.com>
2021-11-22 13:14:43 -0600

    figuring out base components

---
+++ client/src/components/HomepageContainer.js
@@ -0,0 +1,15 @@
+function HomepageContainer() {
+    return (
+        <header>
+            <h1>
+                TRUE CRIME
+            </h1>
+        </header>
+        //landing page component
+        //instruction component
+        //login sign up component
+        //start game component
+    )
+};
+
+export default HomepageContainer;
