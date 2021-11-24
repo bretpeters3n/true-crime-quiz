87dedbd1f78050fc4ba5da2d05e0f48fad3e236a
Basquiat <ememdaniel98@gmail.com>
2021-11-24 13:19:21 -0600

    fixed conflicts, questions getting pulled from the database

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -44,8 +44,8 @@ export default function Game() {
   //   },
   // ];
 
-  const [ renderReady, setRenderReady ] = useState(false)
-  const [ questions, setQuestions ] = useState([]);
+  const [renderReady, setRenderReady] = useState(false)
+  const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
@@ -63,7 +63,7 @@ export default function Game() {
     }
   };
 
-  const getQuestionsFromDB = async() => {
+  const getQuestionsFromDB = async () => {
     const response = await fetch('/api/questions/all');
     const data = await response.json();
     console.log(data)
@@ -78,9 +78,8 @@ export default function Game() {
   }, [])
 
   return (
-
     <>
-      { renderReady === true && (
+      {renderReady === true && (
         <div className="columns-container">
           <motion.div
             className="app"
@@ -91,39 +90,14 @@ export default function Game() {
               duration: 0.3,
               type: "spring",
             }}
+            whileTap={{ scale: 0.6, opacity: 0 }}
           >
             {showScore ? (
               <div className="score-section">
-                You scored {score} out of {questions[0].length}
-
-    <div className="columns-container">
-      <motion.div
-        className="app"
-        initial={{ scale: 0.5, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{
-          delay: 0.1,
-          duration: 0.3,
-          type: "spring",
-        }}
-        whileTap={{ scale: 0.6, opacity: 0 }}
-      >
-        {showScore ? (
-          <div className="score-section">
-            You scored {score} out of {questions.length}
-            <Link to="/game">
-              <Button variant="primary">Play Again!</Button>
-            </Link>
-          </div>
-        ) : (
-          <>
-            <div className="question-section">
-              <div className="question-count">
-                <span>Question {currentQuestion + 1}</span>/{questions.length}
-              </div>
-              <div className="question-text">
-                {questions[currentQuestion].questionText}
-
+                You scored {score} out of {questions.length}
+                <Link to="/game">
+                  <Button variant="primary">Play Again!</Button>
+                </Link>
               </div>
             ) : (
               <>
@@ -132,17 +106,17 @@ export default function Game() {
                     <span>Question {currentQuestion + 1}</span>/{questions.length}
                   </div>
                   <div className="question-text">
-                    {questions[1].questionText}
+                    {questions[currentQuestion].questionText}
                   </div>
                 </div>
                 <div className="answer-section">
-                  {questions[0].answerOptions.map((answerOption) => (
+                  {questions[currentQuestion].answerOptions.map((answerOption) => (
                     <button
                       onClick={() =>
                         handleAnswerOptionClick(answerOption.isCorrect)
                       }
                     >
-                      {questions[0].answerOption.answerText}
+                      {answerOption.answerText}
                     </button>
                   ))}
                 </div>
@@ -150,7 +124,6 @@ export default function Game() {
             )}
           </motion.div>
         </div>
-      )}
+      )};
     </>
-  );
-}
+  )}
