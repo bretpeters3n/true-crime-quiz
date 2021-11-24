9fb363f54516eb927a34beb96c301c21e7c3c613
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-24 11:41:33 -0600

    finalized landing page layout

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -8,16 +8,23 @@ export default function Homepage() {
   return (
     <>
       <div className="columns-container">
-        <div className="app">
-          <div className="align-vertical">
-            Let's get started.
-            <br />
-            <Link to="/game">
-              <Button variant="primary">Start Game!</Button>
-            </Link>
+        <div className="sides">
+          <div className="app">
+            <div className="align-vertical">
+              Let's get started.
+              <br />
+              <Link to="/game">
+                <Button variant="primary">Start Game!</Button>
+              </Link>
+            </div>
           </div>
+          <div className="app large2"></div>
         </div>
-        <div className="app"></div>
+          <div className="app-ins"><h2>Instructions</h2><p>Aliquam pellentesque sodales commodo. Aenean eu interdum enim. Aenean at purus rhoncus, mollis est eu, venenatis magna. Pellentesque posuere placerat lectus, id aliquam sapien varius non. Donec vulputate, orci a iaculis consectetur, ante nulla pretium est, quis accumsan risus diam nec ante. Suspendisse dictum fermentum felis eget lobortis. Phasellus pharetra nisi eros, a imperdiet nisi sollicitudin vel. Nullam id lacinia augue. Ut lacinia leo a diam congue scelerisque. Cras vehicula a velit ac ultrices.</p></div>
+        <div className="sides">
+          <div className="app large">Login/Signup</div>
+          <div className="app"></div>
+          </div>
       </div>
     </>
   );
