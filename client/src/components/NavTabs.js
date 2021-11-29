8020e8358c2c0f431334e3218ad5cb52e2716ed2
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-28 19:02:20 -0600

    added logos, and brand to navbar

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -4,10 +4,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Test from "../Nav-icons/testnavavatar.jpg";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 import { UserContext } from "../utils/UserContext";
-
-
+import logo from "../Nav-icons/tcq-logo-flat.png";
 
 
 function NavTabs({ currentPage, handlePageChange }) {
@@ -15,7 +14,15 @@ function NavTabs({ currentPage, handlePageChange }) {
   return (
     <Navbar bg="light">
       <Container className="navContainer">
-        <Navbar.Brand href="/">true crime quiz</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            width="auto"
+            height="50"
+            className="d-inline-block align-top"
+            alt="True Crime Quiz Logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-right">
@@ -24,8 +31,20 @@ function NavTabs({ currentPage, handlePageChange }) {
             <NavDropdown
               title={
                 <div className="nav-icon">
-                  <img className="thumbnail-img" src={`${!isAuthenticated ? Test : ""}`} onError={(event) => event.target.style.display = 'none'} alt="test pic" />
-                  <img className="thumbnail-img-user" src={`${isAuthenticated ? user.picture : ""}`} onLoad={(event) => event.target.style.display = 'inline-block'} alt="user pic" />
+                  <img
+                    className="thumbnail-img"
+                    src={`${!isAuthenticated ? Test : ""}`}
+                    onError={(event) => (event.target.style.display = "none")}
+                    alt="test pic"
+                  />
+                  <img
+                    className="thumbnail-img-user"
+                    src={`${isAuthenticated ? user.picture : ""}`}
+                    onLoad={(event) =>
+                      (event.target.style.display = "inline-block")
+                    }
+                    alt="user pic"
+                  />
                   &#8964;
                 </div>
               }
@@ -42,6 +61,6 @@ function NavTabs({ currentPage, handlePageChange }) {
       </Container>
     </Navbar>
   );
-};
+}
 
 export default NavTabs;
