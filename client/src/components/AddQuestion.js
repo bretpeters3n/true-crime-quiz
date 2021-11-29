03b13ed7232b563a041e78a651093855f46e543c
Basquiat <ememdaniel98@gmail.com>
2021-11-28 21:00:18 -0600

    hello world, our questions are finally posting right, and the user auth is attached as well, I am going to cry

--- client/src/components/AddQuestion.js
+++ client/src/components/AddQuestion.js
@@ -1,19 +1,24 @@
 import * as React from "react";
 import Button from "react-bootstrap/Button";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { UserContext } from "../utils/UserContext";
+import { useAuth0 } from '@auth0/auth0-react';
 
-// import ReactDOM from "react-dom";
-// import Button from "@mui/material/Button";
-// import { Link } from "react-router-dom";
 
 export default function AddQuestion() {
-  // Here we set two state variables for firstName and lastName using `useState`
+  const { user, isAuthenticated } = useAuth0();
+
   const [questionText, setQuestionText] = useState("");
   const [answerText1, setAnswerText1] = useState("");
   const [answerText2, setAnswerText2] = useState("");
   const [answerText3, setAnswerText3] = useState("");
   const [answerText4, setAnswerText4] = useState("");
+  const [checkBox1, setCheckbox1] = useState(false);
+  const [checkBox2, setCheckbox2] = useState(false);
+  const [checkBox3, setCheckbox3] = useState(false);
+  const [checkBox4, setCheckbox4] = useState(false);
+  const [auth, setAuth] = useState("");
+  
 
   async function writeQuestionToDb() {
     const response = await fetch("/api/questions/new", {
@@ -23,22 +28,22 @@ export default function AddQuestion() {
         answerOptions: [
           {
             answerText: answerText1,
-            isCorrect: true,
+            isCorrect: checkBox1,
           },
           {
             answerText: answerText2,
-            isCorrect: false,
+            isCorrect: checkBox2,
           },
           {
             answerText: answerText3,
-            isCorrect: false,
+            isCorrect: checkBox3,
           },
           {
             answerText: answerText4,
-            isCorrect: false,
+            isCorrect: checkBox4,
           },
         ],
-        auth_id: "google-oauth2|107644464119450733925",
+        auth_id: user.sub,
       }),
       headers: { "Content-Type": "application/json" },
     });
@@ -52,10 +57,8 @@ export default function AddQuestion() {
   }
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { name, value } = e.target;
 
-    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
     if (name === "questionText") {
       return setQuestionText(value);
     } else if (name === "answerText1") {
@@ -66,19 +69,23 @@ export default function AddQuestion() {
       return setAnswerText3(value);
     } else if (name === "answerText4") {
       return setAnswerText4(value);
+    } else if (name === "checkbox1") {
+      return setCheckbox1(true);
+    } else if (name === "checkbox2") {
+      return setCheckbox2(true);
+    } else if (name === "checkbox3") {
+      return setCheckbox3(true);
+    } else if (name === "checkbox4") {
+      return setCheckbox4(true);
     }
-    // return name === "questionText"
-    //   ? setQuestionText(value)
-    //   : setAnswerText1(value);
+
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
     writeQuestionToDb();
 
-    // Alert the user their first and last name, clear the inputs
-    // alert(`Hello ${firstName} ${lastName}`);
+
     setQuestionText("");
     setAnswerText1("");
     setAnswerText2("");
@@ -87,76 +94,83 @@ export default function AddQuestion() {
   };
 
   return (
-    <div className="columns-container">
-      <div className="addQuestion-app">
-        <form className="form">
-          <h3>Add a custom question!</h3>
-          <p className="addQuestionIns">
-            Enter your question, answers, and choose which one will be correct.
-          </p>
-          <textarea
-            className="yourQuestion"
-            value={questionText}
-            name="questionText"
-            onChange={handleInputChange}
-            type="text"
-            placeholder="Your Question"
-          />
-          <div className="sideBySideInputs">
-            <input
-              className="yourAnswer"
-              value={answerText1}
-              name="answerText1"
-              onChange={handleInputChange}
-              type="text"
-              placeholder="Answer 1"
-            />
-            <input type="radio" value="0" name="correct" /> Correct
+    <UserContext.Consumer>
+      {value => {
+        console.log("value", value);
+        console.log("sub", value.sub);
+        return <div className="columns-container">
+          <div className="addQuestion-app">
+            <form className="form">
+              <h3>Add a custom question!</h3>
+              <h4>{value.sub}</h4>
+              <p className="addQuestionIns">
+                Enter your question, answers, and choose which one will be correct.
+              </p>
+              <textarea
+                className="yourQuestion"
+                value={questionText}
+                name="questionText"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="Your Question"
+              />
+              <div className="sideBySideInputs">
+                <input
+                  className="yourAnswer"
+                  value={answerText1}
+                  name="answerText1"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Answer 1"
+                />
+                <input type="radio" value={checkBox1} name="checkbox1" onChange={handleInputChange}/> Correct
+              </div>
+              <div className="sideBySideInputs">
+                <input
+                  className="yourAnswer"
+                  value={answerText2}
+                  name="answerText2"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Answer 2"
+                />
+                <input type="radio" value={checkBox2} name="checkbox2" onChange={handleInputChange}/> Correct
+              </div>
+              <div className="sideBySideInputs">
+                <input
+                  className="yourAnswer"
+                  value={answerText3}
+                  name="answerText3"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Answer 3"
+                />
+                <input type="radio" value={checkBox3} name="checkbox3" onChange={handleInputChange}/> Correct
+                
+              </div>
+              <div className="sideBySideInputs">
+                <input
+                  className="yourAnswer"
+                  value={answerText4}
+                  name="answerText4"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Answer 4"
+                />
+                <input type="radio" value={checkBox4} name="checkbox4" onChange={handleInputChange}/> Correct
+              </div>
+              <Button
+                className="submitBtn"
+                variant="success"
+                type="button"
+                onClick={handleFormSubmit}
+              >
+                Submit
+              </Button>
+            </form>
           </div>
-          <div className="sideBySideInputs">
-            <input
-              className="yourAnswer"
-              value={answerText2}
-              name="answerText2"
-              onChange={handleInputChange}
-              type="text"
-              placeholder="Answer 2"
-            />
-            <input type="radio" value="1" name="correct" /> Correct
-          </div>
-          <div className="sideBySideInputs">
-            <input
-              className="yourAnswer"
-              value={answerText3}
-              name="answerText3"
-              onChange={handleInputChange}
-              type="text"
-              placeholder="Answer 3"
-            />
-            <input type="radio" value="2" name="correct" /> Correct
-          </div>
-          <div className="sideBySideInputs">
-            <input
-              className="yourAnswer"
-              value={answerText4}
-              name="answerText4"
-              onChange={handleInputChange}
-              type="text"
-              placeholder="Answer 4"
-            />
-            <input type="radio" value="3" name="correct" /> Correct
-          </div>
-
-          <Button
-            className="submitBtn"
-            variant="success"
-            type="button"
-            onClick={handleFormSubmit}
-          >
-            Submit
-          </Button>
-        </form>
-      </div>
-    </div>
+        </div>
+      }}
+    </UserContext.Consumer>
   );
 }
